9fef298f4658355a8095a35c96f261cf946ba5b7
Yofi <yofiwidiyanto.fp@gmail.com>
2025-09-08 20:56:22 +0700

    Add files via upload

---
+++ yofi_project1/src/main/java/com/mycompany/yofi_project1/Yofi_project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yofi_project1;
+
+/**
+ *
+ * @author hero
+ */
+public class Yofi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                           Nama   : Yofi Hyuga
+                           NPM    : 1234567890
+                           Alamat : Untid
+                           No_Hp  : 088812345678""");
+    }
+}
